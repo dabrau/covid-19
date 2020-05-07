01480e54af5e9f55bb5835ab6c9a26f9c0c41ff4
Darrell Abrau <darrellabrau@gmail.com>
2020-05-06 22:31:07 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import USAMap from 'react-usa-map';
+import { extent } from 'd3-array';
+import { scaleLinear } from 'd3-scale';
 
 function App() {
+  const [dailyStats, setDailyStats] = useState([]);
+
+  useEffect(() => {
+    fetch('https://covidtracking.com/api/states/daily')
+      .then(resp => resp.json())
+      .then(data => setDailyStats(data))
+  }, []);
+
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+
+  const selectedDayState = dailyStats
+    .filter(stats => stats.date === parseInt(`${yyyy}${mm}${dd}`));
+
+  console.log(selectedDayState)
+
+  const range = extent(selectedDayState, d => d.positive)
+  const colorScale = scaleLinear().domain(range).range(['white', 'blue']);
+
+  const statesConfig = selectedDayState
+    .reduce((acc, usState) => {
+      return {
+        ...acc,
+        [usState.state]: {
+          fill: colorScale(usState.positive)
+        }
+      };
+    }, {});
+
+
+
+  console.log(statesConfig)
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <USAMap customize={statesConfig} />
+      <svg id="legend-svg"></svg>
     </div>
   );
 }
