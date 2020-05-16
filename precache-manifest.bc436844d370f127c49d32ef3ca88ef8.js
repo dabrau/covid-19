dca30e9b09199aad95a8beb68cbf817691d5867e
Darrell Abrau <darrellabrau@gmail.com>
2020-05-16 13:07:04 -0700

    Updates

---
+++ precache-manifest.bc436844d370f127c49d32ef3ca88ef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917f56c47e6155bbc8e1097d7920b175",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "3dbca36038928654acd4",
+    "url": "/covid-19/static/css/main.8b3b5e92.chunk.css"
+  },
+  {
+    "revision": "a32ba7f50c949b36f343",
+    "url": "/covid-19/static/js/2.8b96be0b.chunk.js"
+  },
+  {
+    "revision": "3592b91d760aff2ed9373b19022b95ed",
+    "url": "/covid-19/static/js/2.8b96be0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dbca36038928654acd4",
+    "url": "/covid-19/static/js/main.4b5d51a8.chunk.js"
+  },
+  {
+    "revision": "c59e2f922549c307064d",
+    "url": "/covid-19/static/js/runtime-main.aabceb11.js"
+  }
+]);
